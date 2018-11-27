c00ff50f1bc8194b3a0713cecab12ee5785519a8
htw1 <henryk.woronow@gmail.com>
2018-11-27 14:09:59 +0100

    change list of artist to list of songs

--- app/src/main/java/com/htw/songfinder/network/Services/ServiceLastFm.java
+++ app/src/main/java/com/htw/songfinder/network/Services/ServiceLastFm.java
@@ -9,10 +9,8 @@ import retrofit2.http.Url;
 public interface ServiceLastFm {
 
     String API_LAST_FM_BASE_ERL = "http://ws.audioscrobbler.com/";
-
     String API_LAST_FM_FULL_URL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks";
     String KEY_LAST_FM = "aff4c3b291822c232e266556f9570a23";
-          //?method=artist.gettoptracks & artist
     // 2.0/?method=artist.search & artist=jackson&api_key=aff4c3b291822c232e266556f9570a23&format=json
 
     @GET
